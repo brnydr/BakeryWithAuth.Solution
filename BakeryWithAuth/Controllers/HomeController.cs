6531c381a19ccc24ef75d10e1337b7ea8e2b899b
brnydr <brianwilliamyoder@gmail.com>
2023-11-02 13:59:00 -0700

    add homecontroller

---
+++ BakeryWithAuth/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BakeryWithAuth.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+
+namespace BakeryWithAuth.Controllers;
+    public class HomeController : Controller
+    {
+        private readonly BakeryWithAuthContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public HomeController(UserManager<ApplicationUser> userManager, BakeryWithAuthContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+
+        [HttpGet("/")]
+        public async Task<ActionResult> Index()
+        {
+            Treat[] treats = _db.Treats.ToArray();
+            Flavor[] flavors = _db.Flavors.ToArray();
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            model.Add("treats", treats);
+            model.Add("flavors", flavors);
+            string userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+            return View(model);
+
+        }
+
+    }
