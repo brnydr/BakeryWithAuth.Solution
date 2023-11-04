5cbff26b9086e749053d1edf5a9a96a40b068356
brnydr <brianwilliamyoder@gmail.com>
2023-11-04 12:33:33 -0700

    Add ApplicationUser property to Flavor model

--- BakeryWithAuth/Models/Flavor.cs
+++ BakeryWithAuth/Models/Flavor.cs
@@ -9,5 +9,6 @@ namespace BakeryWithAuth.Models
     [Required(ErrorMessage = "Please enter a flavor type.")]
     public string Type { get; set; }
     public List<FlavorTreat> JoinEntities { get; set; }
+    public ApplicationUser User { get; set; }
   }
 }
